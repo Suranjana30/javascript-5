df24d4e3c94428f33fda049beef4c9e6ea8bdc21
Suranjana30 <90706866+Suranjana30@users.noreply.github.com>
2022-01-29 22:47:11 +0530

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,15 @@
+function redColor(){
+    document.getElementById("myDiv").style.backgroundColor='red';
+}
+function greenColor(){
+    document.getElementById("myDiv").style.backgroundColor='green';
+}
+function blueColor(){
+    document.getElementById("myDiv").style.backgroundColor= 'blue';
+}
+function yellowColor(){
+    document.getElementById("myDiv").style.backgroundColor='yellow';
+}
+function blackColor(){
+    document.getElementById("myDiv").style.backgroundColor='black';
+}
